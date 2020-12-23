cd3d180567124eddab097136ff92427c6e59e77a
Patrick G Silva <patrick.guilhermedasilva@gmail.com>
2020-12-23 13:39:18 -0300

    Configuração para habilitar sessions

--- LoginSystem/Startup.cs
+++ LoginSystem/Startup.cs
@@ -20,6 +20,7 @@ namespace LoginSystem
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSession();
             services.AddControllersWithViews();
             services.AddDbContext<Context>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("Context")));
