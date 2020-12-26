cd8f35fdf1aad349b31e947fe86a5b70fec51c46
Patrick Guilherme da Silva <patrick.guilhermedasilva@gmail.com>
2020-12-25 22:29:48 -0300

    Remoção da data annotations DATATYPE

--- LoginSystem/ViewModel/RegisterLoginModel.cs
+++ LoginSystem/ViewModel/RegisterLoginModel.cs
@@ -14,8 +14,7 @@ namespace LoginSystem.ViewModel
         public string Name { get; set; }
 
         [Display(Name = "E-mail", Description = "E-mail de acesso")]
-        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "E-mail inválido")]
-        [DataType(DataType.EmailAddress, ErrorMessage = "E-mail inválido")]
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "E-mail inválido.")]
         [StringLength(250, MinimumLength = 5, ErrorMessage = "O e-mail deve de 5 a 250 dígitos")]
         [Required(ErrorMessage = "Preencha este campo")]
         public string Email { get; set; }
