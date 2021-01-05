142c877ec8ac1d0cc6bdf8007ef7fe7f12a59602
Patrick Guilherme da Silva <patrick.guilhermedasilva@gmail.com>
2021-01-05 00:46:25 -0300

    Criação de metodo GET e POST de recuperar a senha

--- LoginSystem/Controllers/UsersController.cs
+++ LoginSystem/Controllers/UsersController.cs
@@ -137,7 +137,6 @@ namespace LoginSystem.Controllers
             return View(user);
         }
 
-
         /// <summary>
         /// [GET] Edição de senha de acesso
         /// </summary>
@@ -168,10 +167,45 @@ namespace LoginSystem.Controllers
             return View(passwordEditModel);
         }
 
+        [HttpGet]
+        public IActionResult ForgotPassword() 
+        {
+            if (!String.IsNullOrEmpty(HttpContext.Session.GetString("UserId"))) return RedirectToAction(nameof(UserProfile));
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult ForgotPassword(ForgotPasswordModel forgotPasswordModel) 
+        {
+            if (!UserEmailExists(forgotPasswordModel.Email)) ModelState.AddModelError("Email", "O e-mail não existe");
+            ViewBag.MsgSuccess = null;
+            if (ModelState.IsValid)
+            {
+                ViewBag.MsgSuccess = "Foi enviado um e-mail para você";
+                SendEmail(forgotPasswordModel.Email, "Recuperação de senha", "Utilize o link para recuperar o acesso", "https://localhost:44332/Users/");
+            }
+            return View(forgotPasswordModel);
+        }
+
+        [HttpGet]
+        public IActionResult ForgotPasswordEdit(int id) 
+        {
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult ForgotPasswordEdit(PasswordEditModel passwordEditModel)
+        {
+
+        }
+
         /// <summary>
         /// [POT] Edição de senha de acesso
         /// </summary>
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> UserPasswordEdit(int id, PasswordEditModel passwordEditModel)
         {
             if (id != passwordEditModel.UserId)
@@ -203,8 +237,6 @@ namespace LoginSystem.Controllers
             }
             return View(passwordEditModel);
         }
-
-
 
         /// <summary>
         /// [POST] Editar usuário
@@ -357,75 +389,6 @@ namespace LoginSystem.Controllers
             return View(await _context.Users.ToListAsync());
         }
 
-        // GET: Users/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var user = await _context.Users
-                .FirstOrDefaultAsync(m => m.UserId == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return View(user);
-        }
-
-        // GET: Users/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var user = await _context.Users.FindAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-            return View(user);
-        }
-
-        // POST: Users/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("UserId,Name,Email,Password,BirthData,Genre")] User user)
-        {
-            if (id != user.UserId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(user);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!UserExists(user.UserId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(user);
-        }
-
         // GET: Users/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
@@ -455,6 +418,9 @@ namespace LoginSystem.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        /// <summary>
+        /// Verifica se o usuario existe pelo ID
+        /// </summary>
         private bool UserExists(int id)
         {
             return _context.Users.Any(e => e.UserId == id);
@@ -466,6 +432,8 @@ namespace LoginSystem.Controllers
         /// <param name="email">email a ser verificado</param>
         private bool UserEmailExists(string email)
         {
+            if (String.IsNullOrEmpty(email)) return false;
+
             //Verifica se o email já foi cadastrado
             User searchEmail = _context.Users.Where(m => m.Email.ToUpper().Equals(email.ToUpper())).FirstOrDefault();
             if (searchEmail != null) return true;
